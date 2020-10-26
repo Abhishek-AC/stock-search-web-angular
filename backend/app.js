d048abf0a87786d9ad914ff958237b6547b9f76a
Abhishek Chhabra <02abhishek.chhabra@gmail.com>
2020-10-26 12:33:02 -0700

    converted news date to proper format

--- backend/app.js
+++ backend/app.js
@@ -26,15 +26,14 @@ app.use(bodyParser.json());
 
 // Error handler
 function createError(message) {
-    return {
-      errors: [
-        {
-          message
-        }
-      ]
-    }
+  return {
+    errors: [
+      {
+        message
+      }
+    ]
   }
-
+}
 
 // Endpoint to check if API is working
 app.get('/', (req, res) => {
@@ -100,22 +99,26 @@ app.get('/api/details', async (req, res) => {
   var route;
   var result = {};
   var details = {};
+  const monthNames = ["", "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+  ];
 
   route = 'https://api.tiingo.com/tiingo/daily/' + ticker + '?token=' + token;
-  
+
   var startDate, description, startDateObject;
 
+
   await axios.get(route)
-  .then((response) => {
-    details = {
-      'ticker': response.data.ticker,
-      'name': response.data.name,
-      'exchangeCode': response.data.exchangeCode
-    };
-    result.details = details
-    
-    startDate = response.data.startDate;
-    description = response.data.description;
+    .then((response) => {
+      details = {
+        'ticker': response.data.ticker,
+        'name': response.data.name,
+        'exchangeCode': response.data.exchangeCode
+      };
+      result.details = details
+
+      startDate = response.data.startDate;
+      description = response.data.description;
 
     }, (error) => {
       console.log(error);
@@ -123,59 +126,59 @@ app.get('/api/details', async (req, res) => {
 
   route = 'https://api.tiingo.com/iex?tickers=' + ticker + '&token=' + token;
   await axios.get(route)
-  .then((response) => {
-    var change = response.data[0].last  - response.data[0].prevClose;
-    
-    var currentTimestamp = new Date();
-    var changeMarketStatus = (currentTimestamp - new Date(response.data[0].timestamp))/1000;
+    .then((response) => {
+      var change = response.data[0].last - response.data[0].prevClose;
 
-    var currentMinutes = currentTimestamp.getMinutes().toString().padStart(2, '0');
-    var currentHours = currentTimestamp.getHours().toString().padStart(2, '0');
-    var currentSeconds = currentTimestamp.getSeconds().toString().padStart(2, '0');
-    var date = currentTimestamp.getFullYear() + "-" + (currentTimestamp.getMonth() + 1).toString().padStart(2, '0') + "-" + (currentTimestamp.getDate()).toString().padStart(2, '0') 
+      var currentTimestamp = new Date();
+      var changeMarketStatus = (currentTimestamp - new Date(response.data[0].timestamp)) / 1000;
+
+      var currentMinutes = currentTimestamp.getMinutes().toString().padStart(2, '0');
+      var currentHours = currentTimestamp.getHours().toString().padStart(2, '0');
+      var currentSeconds = currentTimestamp.getSeconds().toString().padStart(2, '0');
+      var date = currentTimestamp.getFullYear() + "-" + (currentTimestamp.getMonth() + 1).toString().padStart(2, '0') + "-" + (currentTimestamp.getDate()).toString().padStart(2, '0')
         + " " + currentHours + ":" + currentMinutes + ":" + currentSeconds;
 
-    var temp = {
-      'last': response.data[0].last,
-      'change': parseFloat(change.toFixed(2)),
-      'changePercentage': parseFloat(((change*100)/(response.data[0].prevClose)).toFixed(2)),
-      'currentTimestamp': date,
-    }
+      var temp = {
+        'last': response.data[0].last,
+        'change': parseFloat(change.toFixed(2)),
+        'changePercentage': parseFloat(((change * 100) / (response.data[0].prevClose)).toFixed(2)),
+        'currentTimestamp': date,
+      }
 
-    var summary = {
-      'highPrice': response.data[0].high,
-      'lowPrice': response.data[0].low,
-      'openPrice': response.data[0].open,
-      'prevClose': response.data[0].prev,
-      'volume': response.data[0].volume,
-      'startDate': startDate,
-      'description': description
-    };
+      var summary = {
+        'highPrice': response.data[0].high,
+        'lowPrice': response.data[0].low,
+        'openPrice': response.data[0].open,
+        'prevClose': response.data[0].prev,
+        'volume': response.data[0].volume,
+        'startDate': startDate,
+        'description': description
+      };
 
-    if (changeMarketStatus > 60) {
-      temp.marketStatus = 'close';
-      temp.lastTimestamp = date;
-      var chartDate = new Date(response.data[0].timestamp);
+      if (changeMarketStatus > 60) {
+        temp.marketStatus = 'close';
+        temp.lastTimestamp = date;
+        var chartDate = new Date(response.data[0].timestamp);
 
-      startDate = chartDate.getFullYear() + "-" + (chartDate.getMonth() + 1).toString().padStart(2, '0') + "-" + chartDate.getDate().toString().padStart(2, '0');
-      startDateObject = chartDate;
-    }
-    else {
-      temp.marketStatus = 'open';
-      summary.midPrice = response.data[0].mid == null ? '-' :  response.data[0].mid;
-      summary.askPrice = response.data[0].askPrice == null ? '-' : response.data[0].askPrice;
-      summary.askSize = response.data[0].askSize == null ? '-' : response.data[0].askSize;
-      summary.bidSize = response.data[0].bidSize == null ? '-' : response.data[0].bidSize;
-      summary.bidPrice = response.data[0].bidPrice == null ? '-': response.data[0].bidPrice;
-      startDate = currentTimestamp.getFullYear() + "-" + (currentTimestamp.getMonth() + 1).toString().padStart(2, '0') + "-" + currentTimestamp.getDate().toString().padStart(2, '0');
-      startDateObject = currentTimestamp;
-    }
-    result.details = Object.assign({},result.details, temp)
-    result.summary = summary;
-  },
-  (error) => {
-    console.log(error);
-  });
+        startDate = chartDate.getFullYear() + "-" + (chartDate.getMonth() + 1).toString().padStart(2, '0') + "-" + chartDate.getDate().toString().padStart(2, '0');
+        startDateObject = chartDate;
+      }
+      else {
+        temp.marketStatus = 'open';
+        summary.midPrice = response.data[0].mid == null ? '-' : response.data[0].mid;
+        summary.askPrice = response.data[0].askPrice == null ? '-' : response.data[0].askPrice;
+        summary.askSize = response.data[0].askSize == null ? '-' : response.data[0].askSize;
+        summary.bidSize = response.data[0].bidSize == null ? '-' : response.data[0].bidSize;
+        summary.bidPrice = response.data[0].bidPrice == null ? '-' : response.data[0].bidPrice;
+        startDate = currentTimestamp.getFullYear() + "-" + (currentTimestamp.getMonth() + 1).toString().padStart(2, '0') + "-" + currentTimestamp.getDate().toString().padStart(2, '0');
+        startDateObject = currentTimestamp;
+      }
+      result.details = Object.assign({}, result.details, temp)
+      result.summary = summary;
+    },
+      (error) => {
+        console.log(error);
+      });
 
   var newsArticles = []
 
@@ -183,13 +186,14 @@ app.get('/api/details', async (req, res) => {
   await axios.get(route)
     .then((response) => {
       var newsDump = response.data.articles;
-      for(var i = 0; i < newsDump.length; ++i) {
+      for (var i = 0; i < newsDump.length; ++i) {
         if (newsDump[i].title != null && newsDump[i].url != null && newsDump[i].urlToImage && newsDump[i].publishedAt != null) {
+          let publishedDate = monthNames[newsDump[i].publishedAt.slice(5,7)] + " " + newsDump[i].publishedAt.slice(8,10) + ", " + newsDump[i].publishedAt.slice(0,4);
           var newsData = {
             'title': newsDump[i].title,
             'url': newsDump[i].url,
             'urlToImage': newsDump[i].urlToImage,
-            'publishedAt': newsDump[i].publishedAt,
+            'publishedAt': publishedDate,
             'source': newsDump[i].source.name,
             'description': newsDump[i].description
           }
@@ -198,54 +202,54 @@ app.get('/api/details', async (req, res) => {
       }
       result.newsArticles = newsArticles
     },
-    (error) => {
-      console.log(error);
-    });
+      (error) => {
+        console.log(error);
+      });
 
   route = 'https://api.tiingo.com/iex/' + ticker + '/prices?startDate=' + startDate + '&resampleFreq=4min&token=' + token;
   await axios.get(route)
     .then((response) => {
       var date_closing = [];
-      response = response.data; 
-      for (var i=0; i<response.length; i++) {
-        var year = parseInt(response[i].date.slice(0,4))
-        var month = parseInt(response[i].date.slice(5,7))
-        var day = parseInt(response[i].date.slice(8,10))
-        var utcDate = Date.UTC(year, month-1, day)
+      response = response.data;
+      for (var i = 0; i < response.length; i++) {
+        var year = parseInt(response[i].date.slice(0, 4))
+        var month = parseInt(response[i].date.slice(5, 7))
+        var day = parseInt(response[i].date.slice(8, 10))
+        var utcDate = Date.UTC(year, month - 1, day)
         date_closing.push([utcDate, response[i].close])
-    }
-    result.summaryTabCharts = date_closing;
+      }
+      result.summaryTabCharts = date_closing;
     },
-    (error) => {
-      console.log(error);
-    });
-  
+      (error) => {
+        console.log(error);
+      });
+
   var startDateLastTwoYears = new Date(new Date(startDateObject).setFullYear(new Date().getFullYear() - 2));
 
   startDateLastTwoYears = startDateLastTwoYears.getFullYear() + "-" + (startDateLastTwoYears.getMonth() + 1).toString().padStart(2, '0') + "-" + startDateLastTwoYears.getDate().toString().padStart(2, '0');
-  
-  route = 'https://api.tiingo.com/tiingo/daily/' + ticker + '/prices?startDate=' + startDateLastTwoYears + '&endDate='+ startDate + '&resampleFreq=monthly&token=' + token;
-  
+
+  route = 'https://api.tiingo.com/tiingo/daily/' + ticker + '/prices?startDate=' + startDateLastTwoYears + '&endDate=' + startDate + '&resampleFreq=monthly&token=' + token;
+
   await axios.get(route)
     .then((response) => {
       var historicalData = response.data;
       var sma_volume = [];
       var sma_ohlc = [];
       for (var i = 0; i < historicalData.length; i++) {
-        var year = parseInt(historicalData[i].date.slice(0,4))
-        var month = parseInt(historicalData[i].date.slice(5,7))
-        var day = parseInt(historicalData[i].date.slice(8,10))
-        var utcDate = Date.UTC(year, month-1, day)
+        var year = parseInt(historicalData[i].date.slice(0, 4))
+        var month = parseInt(historicalData[i].date.slice(5, 7))
+        var day = parseInt(historicalData[i].date.slice(8, 10))
+        var utcDate = Date.UTC(year, month - 1, day)
         sma_ohlc.push([utcDate, historicalData[i].open, historicalData[i].high, historicalData[i].low, historicalData[i].close]);
         sma_volume.push([utcDate, historicalData[i].volume]);
       }
 
       result.sma_volume = sma_volume;
       result.sma_ohlc = sma_ohlc;
-    }, 
-    (error) => {
-      console.log(error);
-    });
+    },
+      (error) => {
+        console.log(error);
+      });
 
   res.send({
     'result': result
@@ -256,22 +260,22 @@ app.get('/api/autocomplete', async (req, res) => {
   var q = req.query.q;
   var token = '2e60e48782d8be49b0f9e7b9a3b627bc50bdc58d';
   var result = [];
-  route = 'https://api.tiingo.com/tiingo/utilities/search?query=' + q + '&token=' + token ;
+  route = 'https://api.tiingo.com/tiingo/utilities/search?query=' + q + '&token=' + token;
   await axios.get(route)
-  .then((response) => {
-    
-    response = response.data;
-    for (var i = 0; i < response.length; ++i) {
-      if (response[i].name != null)
-        result.push({
-          'name': response[i].name,
-          'ticker': response[i].ticker
-        })
-    }
-  },
-  (error) => {
-    console.log(error);
-  });
+    .then((response) => {
+
+      response = response.data;
+      for (var i = 0; i < response.length; ++i) {
+        if (response[i].name != null)
+          result.push({
+            'name': response[i].name,
+            'ticker': response[i].ticker
+          })
+      }
+    },
+      (error) => {
+        console.log(error);
+      });
   res.send({
     'result': result
   });
