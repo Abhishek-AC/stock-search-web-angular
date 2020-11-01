e3519f49ff588ce1263b8e9564494774b168c4f8
Abhishek Chhabra <02abhishek.chhabra@gmail.com>
2020-11-01 14:46:15 -0800

    added a service to get the data from watchlist endpoint

--- frontend/src/app/services/autocomplete.service.ts
+++ frontend/src/app/services/autocomplete.service.ts
@@ -7,7 +7,8 @@ import { debounceTime, map } from 'rxjs/operators';
 })
 export class AutocompleteService {
   apiURLAutoComplete: string = 'http://localhost:3000/api/autocomplete?q=';
-  apiURLDetails: string = 'http://localhost:3000/api/details?ticker='
+  apiURLDetails: string = 'http://localhost:3000/api/details?ticker=';
+  apiWatchlist: string = 'http://localhost:3000/api/watchlist?';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -31,6 +32,27 @@ export class AutocompleteService {
     var response = this.httpClient.get(this.apiURLDetails + ticker)
     .pipe(
       debounceTime(400),
+      map(
+        (data: any) => {
+          return (
+            data.length != 0 ? data as any[] : [{"error": "Invalid key"} as any]
+          );
+
+        }
+      )
+    );
+    return response;
+  }
+  getWatchlistData(tickers: string[]) {
+    var quertyString = "";
+    for (var i in tickers) {
+      quertyString += "q=" + tickers[i] + "&";
+    }
+    quertyString = quertyString.slice(0, -1);
+    var route = this.apiWatchlist + quertyString;
+    var response = this.httpClient.get(route)
+    .pipe(
+      debounceTime(200),
       map(
         (data: any) => {
           return (
