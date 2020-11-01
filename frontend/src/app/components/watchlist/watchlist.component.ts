fea2b8f53a4286a5101c66da5c56f12b3740b6f8
Abhishek Chhabra <02abhishek.chhabra@gmail.com>
2020-11-01 14:45:56 -0800

    added the watchist componenent and functionality

--- frontend/src/app/components/watchlist/watchlist.component.ts
+++ frontend/src/app/components/watchlist/watchlist.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-
+import { debounceTime } from 'rxjs/operators';
+import { AutocompleteService } from 'src/app/services/autocomplete.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-watchlist',
   templateUrl: './watchlist.component.html',
   styleUrls: ['./watchlist.component.css']
 })
 export class WatchlistComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(private _http: AutocompleteService, private _router: Router) { }
+  isLoading: boolean = true;
+  watchlistLocalStorage: Object = {};
+  watchlistKeys : string[];
+  watchlistPriceData : Object = {};
 
   ngOnInit(): void {
+
+    this.watchlistLocalStorage = JSON.parse(localStorage.getItem("Watchlist"));
+
+    if (this.watchlistLocalStorage != null && Object.keys(this.watchlistLocalStorage).length!= 0)  {
+      this.watchlistKeys = Object.keys(this.watchlistLocalStorage);
+      // call the service
+      this._http.getWatchlistData(this.watchlistKeys)
+      .pipe(
+        debounceTime(200))
+        .subscribe(data => {
+          if ("error" in data) {
+            console.log('In error')
+            this.isLoading = false;
+          }
+          else {
+            this.watchlistPriceData = data;
+            this.isLoading = false;
+          }
+        })
+    }
+    else {
+      this.isLoading = false;
+    }
   }
 
+  removeTickerFromWatchList(ticker) {
+    delete this.watchlistLocalStorage[ticker];
+    localStorage.setItem("Watchlist", JSON.stringify(this.watchlistLocalStorage));
+  }
+
+  navigateToDetails(ticker) {
+    console.log(ticker);
+    this._router.navigate(['/details', ticker]);
+  }
 }
